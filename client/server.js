1049201c0b52fba3e04a8ce1680290e4c1787f3e
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-05-11 14:55:16 +0300

    remove type module

--- client/server.js
+++ client/server.js
@@ -1,16 +1,13 @@
-import express from 'express'
-import { dirname, join } from 'path'
-import { fileURLToPath } from 'url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(9000, () => {
     console.log(`Server is listening on port 9000`)
