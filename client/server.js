c7fe459b9bc8c84e6b718a9daa2d3014612de544
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2022-05-12 09:27:27 +0300

    add client https server

--- client/server.js
+++ client/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
+const https = require('https')
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -8,7 +10,26 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+let isHttps = false
+let key, cert
+try {
+    key = fs.readFileSync('/etc/letsencrypt/live/psy-forum-sno.ru/privkey.pem')
+    cert = fs.readFileSync('/etc/letsencrypt/live/psy-forum-sno.ru/fullchain.pem')
+    isHttps = true
+} catch (error) {
+    console.log(error)
+}
 
-app.listen(9000, () => {
+let server = app
+if (isHttps) {
+    console.log('https')
+    server = https.createServer({
+        key,
+        cert
+    }, app)
+}
+
+
+server.listen(9000, () => {
     console.log(`Server is listening on port 9000`)
 });
